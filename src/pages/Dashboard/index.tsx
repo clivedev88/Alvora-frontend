911d2ce3e99c114a4cc1ce2645cbca37369062e2
clivedev88m <clive.dev88@gmail.com>
2025-07-23 13:10:56 -0300

    Testando dashboard

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,12 @@
+// import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <div>
+      <h1>Bem-vindo ao Dashboard</h1>
+      <p>Visualize suas limpezas anteriores e próximas aqui.</p>
+    </div>
+  )
+}
+
+export default Dashboard
